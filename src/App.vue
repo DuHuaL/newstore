b234d24c3d224f6795cac3631264ed52afbfc487
DuHuaL <469580396@qq.com>
2020-05-26 22:31:32 +0800

    搭建主页面

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <my-header></my-header>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active">
+              <a href="#">Overview
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">Reports</a>
+            </li>
+            <li>
+              <a href="#">Analytics</a>
+            </li>
+            <li>
+              <a href="#">Export</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <!-- 列表 -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// 导入头部组件
+import MyHeader from '@/components/myheader.vue';
 export default {
-  name: 'App'
+  name: 'App',
+  // 注册局部组件
+  components: {
+    MyHeader
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+// 根组件
 import App from './App';
 import router from './router';
+// 导入bootstrap样式
+import 'bootstrap/dist/css/bootstrap.css';
+// 导入css样式
+import '@/assets/css/index.css';
 
 Vue.config.productionTip = false;
 
