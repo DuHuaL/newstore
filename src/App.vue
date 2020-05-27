9b9770a4071d68d927d9f5810e2500246b7ed20b
DuHuaL <469580396@qq.com>
2020-05-27 12:53:13 +0800

    侧边栏页面

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,8 @@
     <my-header></my-header>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active">
-              <a href="#">Overview
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">Reports</a>
-            </li>
-            <li>
-              <a href="#">Analytics</a>
-            </li>
-            <li>
-              <a href="#">Export</a>
-            </li>
-          </ul>
-        </div>
+        <!-- // 侧边栏 -->
+        <my-sidebar></my-sidebar>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <!-- 列表 -->
         </div>
@@ -32,11 +16,13 @@
 <script>
 // 导入头部组件
 import MyHeader from '@/components/myheader.vue';
+import MySidebar from '@/components/mysidebar.vue';
 export default {
   name: 'App',
   // 注册局部组件
   components: {
-    MyHeader
+    MyHeader,
+    MySidebar
   }
 };
 </script>
